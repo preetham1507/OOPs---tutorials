19e172caf5d65b885fadc91982254b4297c50dca
preetham1507 <preethamsrinivasgowda@gmail.com>
2025-09-06 03:04:47 +0530

    sigin and signup done

--- oops_proj.py
+++ oops_proj.py
@@ -14,15 +14,44 @@ class chatbook:
                                    5. press any other key to exit   -   """)
         
         if user_input=="1":
-            pass
+            self.signup()
+             
         elif user_input=="2":
-            pass
+            self.signin()
+
         elif user_input=="3":
             pass
         elif user_input=="4":
             pass
         else:
             exit()
+
+    def signup(self):
+        email = input("enter your user email - ")
+        pwd = input("setup you password - ")
+        self.username = email
+        self.password = pwd
+        print("You have signed-up successfully")
+        print('\n')
+        self.menu()
+
+    def signin(self):
+        if self.username == "" and self.password == "":
+            print("Please signup, press 1")
+
+        else:
+            email = input("enter your user email - ")
+            pwd = input("setup you password - ")
+            if self.username == email and self.password==pwd:
+                print("You are signed in")
+                self.loggedin=True
+
+            else:
+                print("You input correct credentials")
+
+            print('\n')
+            self.menu()
+
 
 
 obj = chatbook()
